703999f3224949ccc349d235270597d6ae92e74f
Augusto <mascittija@gmail.com>
2017-10-16 20:50:51 -0300

    terminado

--- tp2/tp2.pl
+++ tp2/tp2.pl
@@ -31,10 +31,6 @@ direccion(horizontal).
 elemento(o).
 elemento(~).
 
-%dameElEnesimo(Arrego, N, X)
-dameElEnesimo([X|_], 1, X).
-dameElEnesimo([_|Xs], N, Res) :- N > 0, length([_|Xs], Long), N =< Long, N2 is N - 1, dameElEnesimo(Xs, N2, Res).
-
 %colocarBarco(+Barco,+Direccion,+?Tablero,+Fila,+Columna,-TableroNuevo)
 colocarBarco(0,_,_,_,_).
 colocarBarco(B,D,T,F,C) :- B > 0, direccion(D), vertical = D, B1 is B - 1, F1 is F + 1, contenido(T,F,C,X1), X1 = o, colocarBarco(B1,D,T,F1,C).
@@ -54,13 +50,9 @@ igualSalvoIesimo(Xs,I,Ys) :- append(Principio, [_|Ultimos], Xs), append(Principi
 %------------------Predicados a definir:------------------%
 
 %contenido(+?Tablero, ?Fila, ?Columna, ?Contenido)
-%contenido(T,F,C,X) :- enRango(T,F,C), dameElEnesimo(T,F,Fila), dameElEnesimo(Fila,C,X).
 contenido(T,F,C,X) :- enRango(T,F,C), nth1(F,T,Fila), nth1(C,Fila,X).
 
 %disponible(+Tablero, ?Fila, ?Columna)
-%disponible(T,F,C) :- contenido(T,F,C,X1), var(X1), adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,X2), var(X2).
-%disponible(T,F,C) :- contenido(T,F,C,X1), var(X1), setof(var(X2), (adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,X2)), Set), length(Set, Long), Long = 1.
-%disponible(T,F,C,N) :- contenido(T,F,C,X1), var(X1), setof(var(X2), (adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,X2)), Set), length(Set, Long), Long = 1, N = Set.
 disponible(T,F,C) :- contenido(T,F,C,X1), var(X1), not(not(not((adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,X2), nonvar(X2))))).
 
 %puedoColocar(+CantPiezas, ?Direccion, +Tablero, ?Fila, ?Columna)
@@ -74,7 +66,6 @@ ubicarBarcos([Bcantidad|Bs],T) :- recorrerTablero(T,F,C), puedoColocar(Bcantidad
 ubicarBarcos([Bcantidad|Bs],T) :- recorrerTablero(T,F,C), puedoColocar(Bcantidad,vertical,T,F,C), not(puedoColocar(Bcantidad,horizontal,T,F,C)), colocarBarco(Bcantidad,vertical,T,F,C), ubicarBarcos(Bs,T) .
 
 %completarConAgua(+?Tablero)
-%completarConAgua(T) :- not(not((recorrerTablero(T,F1,C1), contenido(T,F1,C1,X1), var(X1)))), recorrerTablero(T,F,C), contenido(T,F,C,~), completarConAgua(T).
 completarConAgua(T) :- maplist(maplist(agua),T,T).
 
 %golpear(+Tablero, +NumFila, +NumColumna, -NuevoTab)
