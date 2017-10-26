a3f28fd584e642257d15bc629050354ccb1f0fdf
amascitti <amascitti@zetti.com.ar>
2017-10-26 16:07:56 -0300

    arreglos tp2

---
+++ tp2/tp2.pl
@@ -0,0 +1,169 @@
+%------------------Predicados predefinidos:------------------%
+
+%fliplength(?Longitud, ?Lista)
+fliplength(N, L) :- length(L, N).
+
+%matriz(?Matriz, ?Filas, ?Columnas)
+matriz(M, F, C) :- length(M, F), maplist(fliplength(C), M).
+
+%dif1(+N1, ?N2)
+dif1(N1, N2) :- N2 is N1 + 1.
+dif1(N1, N2) :- N2 is N1 - 1.
+
+%adyacente(+F1, +C1, ?F2, ?C2)
+adyacente(F1,C1,F1,C2) :- dif1(C1,C2).
+adyacente(F1,C1,F2,C1) :- dif1(F1,F2).
+adyacente(F1,C1,F2,C2) :- dif1(C1,C2), dif1(F1,F2).
+
+%enRango(+Matriz, +Fila, +Columna)
+enRango([Fila|Filas], F, C) :- F > 0, C > 0, length([Fila|Filas], FMax), F =< FMax, length(Fila, CMax), C =< CMax.
+
+%adyacenteEnRango(+Tablero, +F1, +C1, ?F2, ?C2)
+adyacenteEnRango(T,F1,C1,F2,C2) :- adyacente(F1,C1,F2,C2), enRango(T,F2,C2).
+
+%------------------Funciones auxiliares:------------------%
+
+%colocarBarco(+Barco,+Direccion,+?Tablero,+Fila,+Columna,-TableroNuevo)
+colocarBarco(0,_,_,_,_).
+colocarBarco(B,D,T,F,C) :-
+	B > 0,
+	D = vertical,
+	B1 is B - 1,
+	F1 is F + 1,
+	contenido(T,F,C,o),
+	colocarBarco(B1,D,T,F1,C).
+colocarBarco(B,D,T,F,C) :-
+	B > 0,
+	D = horizontal,
+	B1 is B - 1,
+	C1 is C + 1,
+	contenido(T,F,C,o),
+	colocarBarco(B1,D,T,F,C1).
+
+%recorrerTablero(+?Tablero,?Fila,?Columna)
+recorrerTablero(T,F,C) :-
+	length(T,Filas),
+	nth1(1,T,Fila),
+	length(Fila,Columnas),
+	N is Filas + Columnas,
+	between(2,N,Suma),
+	between(1,Suma,F),
+	C is Suma - F,
+	C \= 0,
+	enRango(T,F,C).
+
+%aguaSiEstoyLibre(?X,?Y)
+aguaSiEstoyLibre(X,~) :- var(X).
+aguaSiEstoyLibre(X,X) :- nonvar(X).
+
+%igualSalvoIesimo(+Xs,+I,-Ys)
+igualSalvoIesimo(Xs,I,Ys) :-
+	append(Principio, [_|Ultimos], Xs),
+	append(Principio, [_|Ultimos], Ys),
+	length(Principio, N),
+	N is I - 1.
+
+%------------------Predicados a definir:------------------%
+
+%contenido(+?Tablero, ?Fila, ?Columna, ?Contenido)
+contenido(T,F,C,X) :- nth1(F,T,Fila), nth1(C,Fila,X).
+
+%disponible(+Tablero, ?Fila, ?Columna)
+disponible(T,F,C) :-
+	contenido(T,F,C,X1),
+	var(X1),
+	forall((adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,X2)), nonvar(X2)).
+
+%	not(not(not((adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,X2), nonvar(X2))))).
+%disponible(T,F,C) :- contenido(T,F,C,X1), var(X1), forall(adyacenteEnRango(T,F,C,F1,C1),(contenido(T,F1,C1,X2), nonvar(X2))).
+
+%puedoColocar(+CantPiezas, ?Direccion, +Tablero, ?Fila, ?Columna)
+puedoColocar(0,_,_,_,_).
+puedoColocar(N,D,T,F,C) :- 
+	N  > 0,
+	D = vertical,
+	disponible(T,F,C),
+	N1 is N - 1,
+	F1 is F + 1,
+	puedoColocar(N1,D,T,F1,C).
+puedoColocar(N,D,T,F,C) :-
+	N  > 0,
+	D = horizontal,
+	disponible(T,F,C),
+	N1 is N - 1,
+	C1 is C + 1,
+	puedoColocar(N1,D,T,F,C1).
+
+%ubicarBarcos(+Barcos, +?Tablero)
+ubicarBarcos([],_).
+ubicarBarcos([Bcantidad|Bs],T) :-
+	recorrerTablero(T,F,C),
+	puedoColocar(Bcantidad,horizontal,T,F,C),
+	colocarBarco(Bcantidad,horizontal,T,F,C),
+	ubicarBarcos(Bs,T) .
+ubicarBarcos([Bcantidad|Bs],T) :-
+	recorrerTablero(T,F,C),
+	puedoColocar(Bcantidad,vertical,T,F,C),
+	not(puedoColocar(Bcantidad,horizontal,T,F,C)),
+	colocarBarco(Bcantidad,vertical,T,F,C),
+	ubicarBarcos(Bs,T) .
+
+%completarConAgua(+?Tablero)
+completarConAgua(T) :- maplist(maplist(aguaSiEstoyLibre),T,T).
+
+%golpear(+Tablero, +NumFila, +NumColumna, -NuevoTab)
+golpear(T,F,C,T) :- contenido(T,F,C,~).
+golpear(T,F,C,Tnew) :-
+	igualSalvoIesimo(T,F,Tnew),
+	nth1(F,T,FilaVieja),
+	nth1(F,Tnew,FilaNueva),
+	igualSalvoIesimo(FilaVieja,C,FilaNueva),
+	nth1(C,FilaNueva,~),
+	not(contenido(T,F,C,~)).
+
+
+% Completar instanciación soportada y justificar.
+% ----------------------------------------------------------------------------------------------------------- %
+% El predicado golpear necesita Tablre, Fila, Columna instanciados, con lo cual este tambien pues los utiliza
+% Luego Resultado siempre está bien definido, con lo cual puede venir instanciado o no.
+% ----------------------------------------------------------------------------------------------------------- %
+%atacar(+Tablero, +Fila, +Columna, ?Resultado, -NuevoTab)
+atacar(T,F,C,agua,T) :- contenido(T,F,C,~).
+atacar(T,F,C,tocado,Tnew) :-
+	contenido(T,F,C,o),
+	golpear(T,F,C,Tnew),
+	not(not((adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,o)))).
+atacar(T,F,C,hundido,Tnew) :-
+	contenido(T,F,C,o),
+	golpear(T,F,C,Tnew),
+	not(not(not((adyacenteEnRango(T,F,C,F1,C1), contenido(T,F1,C1,o))))).
+
+%------------------Tests:------------------%
+
+test(1) :- matriz(M,2,3), adyacenteEnRango(M,2,2,2,3).
+test(2) :- matriz(M,2,3), setof((F,C), adyacenteEnRango(M,1,1,F,C), [ (1, 2), (2, 1), (2, 2)]).
+test(3) :- matriz(M,3,3), contenido(M,1,1,o), not(disponible(M,1,1)).
+test(4) :- matriz(M,3,3), contenido(M,1,1,o), not(disponible(M,1,2)).
+test(5) :- matriz(M,3,3), contenido(M,1,1,o), not(disponible(M,2,1)).
+test(6) :- matriz(M,3,3), contenido(M,1,1,o), not(disponible(M,2,2)).
+test(7) :- matriz(M,3,3), contenido(M,1,1,o), disponible(M,1,3).
+test(8) :- matriz(M,3,3), contenido(M,1,1,o), disponible(M,2,3).
+test(9) :- matriz(M,3,3), contenido(M,1,1,o), disponible(M,3,3).
+test(10) :- matriz(M,3,3), contenido(M,1,1,o), disponible(M,3,2).
+test(11) :- matriz(M,3,3), contenido(M,1,1,o), disponible(M,3,1).
+test(12) :- matriz(M,2,2), puedoColocar(2,horizontal,M,1,1).
+test(13) :- matriz(M,2,2), puedoColocar(2,horizontal,M,2,1).
+test(14) :- matriz(M,2,2), not(puedoColocar(2,horizontal,M,2,2)).
+test(15) :- matriz(M,2,2), not(puedoColocar(2,horizontal,M,1,2)).
+test(16) :- matriz(M,3,2), setof(M, (ubicarBarcos([2,1],M), completarConAgua(M)), Set),
+    Set = [[[o, o], [~, ~], [o, ~]], [[o, o], [~, ~], [~, o]], [[o, ~], [~, ~], [o, o]], [[~, o], [~, ~], [o, o]]].
+test(17) :- matriz(M,3,3), completarConAgua(M), 
+    M = [[~, ~, ~], [~, ~, ~], [~, ~, ~]].
+test(18) :- matriz(M,3,3), contenido(M,1,1,o), contenido(M,2,2,o), contenido(M,3,3,o), completarConAgua(M),
+    M = [[o, ~, ~], [~, o, ~], [~, ~, o]].
+test(19) :- atacar([[o, o, ~], [~, ~, ~], [~, ~, o]],1,1,Res,T), T = [[~, o, ~], [~, ~, ~], [~, ~, o]], Res = tocado.
+test(20) :- atacar([[o, o, ~], [~, ~, ~], [~, ~, o]],2,2,Res,T), T = [[o, o, ~], [~, ~, ~], [~, ~, o]], Res = agua.
+test(21) :- atacar([[o, o, ~], [~, ~, ~], [~, ~, o]],3,3,Res,T), T = [[o, o, ~], [~, ~, ~], [~, ~, ~]], Res = hundido.
+tests :- forall(between(1,21,N), test(N)). % Cambiar el 2 por la cantidad de tests que tengan.
+
+
