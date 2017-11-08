3cac705a1d50d9c4c536be7b094a37b2ae4b1859
Julio <mascittija@github.com>
2017-11-08 01:39:28 -0300

    faltan integradoes

--- prolog/practica.pl
+++ prolog/practica.pl
@@ -67,14 +67,9 @@ reverse([X|Xs],Ls) :- reverse(Xs,Ys), concatenar(Ys,[X],Ls).
 %maxList(+L,-M)
 maxList([X],X).
 maxList([X|Xs],M) :- maxList(Xs,X1), M is max(X,X1).
-%maxList([X|Xs],X) :- M =< X, maxList(Xs,M).
-
 %minList(+L,-M)
 minList([X],X).
 minList([X|Xs],M) :- minList(Xs,X1), M is min(X,X1).
-
-
-
 
 %IV)
 %prefijo(?P,+L)
@@ -89,10 +84,8 @@ sufijo(Xs,L) :- concatenar(_,Xs,L), Xs \= [].
 sublista([],[]).
 sublista(L,Xs) :- sufijo(P,Xs), prefijo(L,P), Xs \= [].
 
-
 %VI)
 pertenece(X,L) :- sublista([X],L).
-
 
 % --------------------------------------------- %
 % ---------------- Ejercicio 6 ---------------- %
@@ -125,16 +118,12 @@ desde(X,X).
 desde(X,Y) :- nonvar(Y), Y > X.
 desde(X,Y) :- var(Y), N is X + 1, desde(N,Y).
 
-
 % --------------------------------------------- %
 % ---------------- Ejercicio 9 ---------------- %
 % --------------------------------------------- %
-
 %interseccion(+L1, +L2, -L3)
 interseccion(_,[],[]).
 interseccion([],_,[]).
-%interseccion([X|Xs], [Y|Ys], Ls) :- X \= Y, interseccion([X|Xs],Ys,Ls).
-%interseccion([X|Xs], [X|Ys], [X|Ls]) :- interseccion(Xs,Ys,Ls).
 interseccion([X|Xs], L, [X|Ys]) :- pertenece(X,L), borrar(L,X,L2), borrar(Xs,X,L3), interseccion(L3,L2,Ys).
 interseccion([X|Xs], L, Ys) :- not(pertenece(X,L)), borrar(Xs,X,L3), interseccion(L3,L,Ys).
 
@@ -153,30 +142,20 @@ sacarDuplicados([X|Xs],[X|Ys]) :- borrar(Xs,X,Ls), sacarDuplicados(Ls,Ys).
 
 %reparto(+L, +N, -Llistas)
 reparto(Xs, 1, [Xs]).
-reparto([],N,[[]|Ys]) :- N > 1, N1 is N - 1, reparto([],N1,Ys). 
-reparto([X|Xs], N, [[X]|Ys]) :- N > 1, N1 is N - 1, reparto(Xs,N1,Ys).
-
-%reparto(Xs,N,Ys) :- length(Ys,N), forall(member(M,Ys), sublista(M,Xs)), aplanar(Ys,Xs).
+%reparto([],N,[[]|Ys]) :- N > 1, N1 is N - 1, reparto([],N1,Ys). 
+reparto(L, N,[L1|Ys]) :- N > 1, append(L1,L2,L), N1 is N - 1, reparto(L2,N1,Ys).
 
 %repartoSinVacias(+L, -Llistas)
-%reparto(Xs, N, Ys) :- length(Ys,.
-%reparto([],N,[[]|Ys]) :- N > 1, N1 is N - 1, reparto([],N1,Ys). 
-%reparto([X|Xs], N, [[X]|Ys]) :- N > 1, N1 is N - 1, reparto(Xs,N1,Ys).
-
+repartoSinVacias([], [[]]).
+repartoSinVacias(Xs,[L1|Ls]) :- length(Xs,N), N > 0, append(L1,L2,Xs), L1 \= [], repartoSinVacias(L2,Ys), borrar(Ys,[],Ls).
 
 % --------------------------------------------- %
 % ---------------- Ejercicio 10 --------------- %
 % --------------------------------------------- %
-
 %intercalar(L1, L2, L3)
-intercalar(L1, L2, L3) :- intercalarAux(1,L1,L2,L3).
-
-%intercalarAux(N,L1,L2,L3)
-intercalarAux(_,[],L2,L2).
-intercalarAux(_,L1,[],L1).
-intercalarAux(1,[X|Xs],L2,[X|L3]) :- L2 \= [], intercalarAux(2,Xs,L2,L3).
-intercalarAux(2,L1,[X|Xs],[X|L3]) :- L1 \= [], intercalarAux(1,L1,Xs,L3).
-
+intercalar([],L2,L2).
+intercalar(L1,[],L1) :- L1 \= [].
+intercalar([X|Xs],[Y|Ys], [X,Y|Ls]) :- intercalar(Xs,Ys,Ls).
 
 % --------------------------------------------- %
 % ---------------- Ejercicio 11 --------------- %
@@ -184,12 +163,9 @@ intercalarAux(2,L1,[X|Xs],[X|L3]) :- L1 \= [], intercalarAux(1,L1,Xs,L3).
 
 % PREGUNTAR
 
-
-
 % --------------------------------------------- %
 % ---------------- Ejercicio 12 --------------- %
 % --------------------------------------------- %
-
 vacio(nill).
 
 raiz(bin(_,R,_),R).
@@ -200,28 +176,85 @@ altura(bin(I,_,D), Res) :- altura(I,N1), altura(D,N2), Res is 1 + max(N1,N2).
 cantidadDeNodos(nill,0).
 cantidadDeNodos(bin(I,_,D), Res) :- cantidadDeNodos(I,N1), cantidadDeNodos(D,N2), Res is 1 + N1 + N2.
 
-
-
 % --------------------------------------------- %
 % ---------------- Ejercicio 13 --------------- %
 % --------------------------------------------- %
-
 %inorder(+AB, -Lista)
 inorder(nill,[]).
 inorder(bin(I,R,D), Res) :- inorder(I,Is), inorder(D,Ds), concatenar(Is,[R],L), concatenar(L,Ds,Res).
 
-
-
 % --------------------------------------------- %
 % ---------------- Ejercicio 14 --------------- %
 % --------------------------------------------- %
-
 coprimos(X,Y) :- desde(2,N), between(2,N,X), Y is N - X, Y \= 1, 1 is gcd(X,Y).
-
-
 
 % --------------------------------------------- %
 % ---------------- Ejercicio 15 --------------- %
 % --------------------------------------------- %
+listaHasta(_,[]).
+listaHasta(Cota, [N|Ls]) :- between(0,Cota,N), listaHasta(Cota, Ls).
 
-%cuadradoSemiLatino(N,X) :- desde(0,I), length(X,N), forall(member(M,X), length(M,N)). 
+cumplen([],_,_).
+cumplen([X|Xs],I,Lon) :- length(X,Lon), listaHasta(I,X), sumlist(X,I), cumplen(Xs,I,Lon).
+
+cuadradoSemiLatino(N,L) :- N > 0, desde(0,I), length(L,N), cumplen(L,I,N). 
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 16 --------------- %
+% --------------------------------------------- %
+%I)
+esCompatible(A,B,C) :- A > 0, A < B + C, Diferencia is B - C, A > abs(Diferencia).
+
+esTrianulo(tri(A,B,C)) :- esCompatible(A,B,C), esCompatible(B,A,C), esCompatible(C,A,B).
+
+%II)
+%perimetro(?T,?P)
+perimetro(tri(A,B,C),P) :- desde(3,P), between(1,P,A), between(1,P,B), between(1,P,C), esTrianulo(tri(A,B,C)), P is A + B + C.
+
+perimetroSinCongruentes(tri(A,B,C),P) :- desde(3,P), between(1,P,A), between(1,A,B), between(1,B,C), esTrianulo(tri(A,B,C)), P is A + B + C.
+
+%III)
+%triangulo(-T)
+triangulo(T) :- desde(0,P), perimetro(T,P).
+
+trianguloSinConcruentes(T) :- desde(0,P), perimetroSinCongruentes(T,P).
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 17 --------------- %
+% --------------------------------------------- %
+diferenciaSimetrica([],L2,L2).
+diferenciaSimetrica(L1,[],L1) :- L1 \= [].
+diferenciaSimetrica([X|Xs], L2, L3) :- member(X,L2), borrar(L2,X,Ls), diferenciaSimetrica(Xs,Ls,L3).
+diferenciaSimetrica([X|Xs], L2, [X|L3]) :- not(member(X,L2)), diferenciaSimetrica(Xs,L2,L3).
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 18 --------------- %
+% --------------------------------------------- %
+%I) 
+%II)
+%III)
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 19 --------------- %
+% --------------------------------------------- %
+
+diff(L1,L2,D) :- sumlist(L1,N1), sumlist(L2,N2), D is abs(N1 - N2).
+
+corteMasParejo(L1, L2, L3) :- append(L2,L3,L1), diff(L2,L3,Dif),  not((append(L4,L5,L1), diff(L4,L5,Dif2), Dif2 < Dif)).
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 21 --------------- %
+% --------------------------------------------- %
+
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 22 --------------- %
+% --------------------------------------------- %
+
+
+% --------------------------------------------- %
+% ---------------- Ejercicio 23 --------------- %
+% --------------------------------------------- %
+
+
+
